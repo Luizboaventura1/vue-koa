375f721c7239679369d4189d2d9386c82d7ce2da
Luiz Henrique <luizboaventura24@gmail.com>
2024-03-16 19:04:24 -0300

    add packages

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="w-screen h-screen flex justify-center items-center">
+    <Toast :state="state" :title="title" />
+    <button
+      @click="toggle"
+      class="text-lg bg-blue-600 m-3 rounded-md p-2 text-white"
+    >
+      Toggle
+    </button>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Toast from "./components/Toast/Toast.vue";
+
+let state = ref<boolean>(false);
+let title = ref<string>("Error!");
+const toggle = () => (state.value = !state.value);
+
+const test = () => {
+  const show = (msg: string): void => {
+    console.log(msg);
+  }
+
+  return {
+    show
+  }
+}
+
+const test1 = test()
+test1.show('opa')
+</script>
+
+<style scoped></style>
---
+++ src/components/Toast/Toast.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    v-if="props.state"
+    class="w-[300px] h-[40px] bg-zinc-800 rounded-md text-white p-4 flex items-center"
+  >
+    {{ props.title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{ state: Boolean; timeout?: Number; title: String }>();
+</script>
+
+<style scoped></style>
