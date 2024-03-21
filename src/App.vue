c9413945899a751ddcb6f8724c78c9fc8c8d7c81
Luiz Henrique <luizboaventura24@gmail.com>
2024-03-21 08:20:51 -0300

    Refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>
-
+  <div
+    class="w-screen h-screen bg-primary-color flex justify-center items-center"
+  >
+    <button
+      class="text-zinc-900 font-bold rounded-md px-9 py-3 bg-emerald-600 hover:bg-emerald-500 transition-colors duration-300"
+      @click="toast.add({ title: 'My toast', timeout: 2000 })"
+    >
+      Show
+    </button>
   </div>
+  <Toast :toast />
 </template>
 
 <script setup lang="ts">
+import useToast from "./composables/useToast";
+import Toast from "./components/Notification/Toast.vue";
 
+const toast = useToast();
 </script>
-
-<style scoped>
-
-</style>
--- src/components/Notification/Toast.vue
+++ src/components/Notification/Toast.vue
@@ -1,36 +1,25 @@
 <template>
-  <div class="absolute right-3 top-3 overflow-hidden">
-    <TransitionGroup name="list" tag="ul" class="grid gap-y-1 p-1">
+  <div class="absolute right-0 top-0 h-full p-3">
+    <TransitionGroup name="list" tag="ul" class="p-1 toasts h-full overflow-hidden">
       <li
         v-for="(toast, index) in props.toast.toastList.value"
         :key="index"
-        class="w-[300px] h-full min-h-[50px] relative bg-primary-color border border-zinc-600 shadow rounded-md text-text-primary-color p-4 flex items-center overflow-hidden"
+        class="w-[300px] h-[55px] bg-primary-color border border-zinc-600 mb-1 shadow rounded-md text-text-primary-color p-4 flex items-center overflow-hidden"
         :class="props.toastStyle"
       >
         {{ toast.title }}
-
-        <div class="absolute bottom-0 left-0 w-full h-[3px]">
-          <div
-            class="bar h-full"
-            :style="{
-              animationDuration: `${toast.timeout ? toast.timeout : 2000}ms`,
-            }"
-          ></div>
-        </div>
       </li>
     </TransitionGroup>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from "vue";
 import type { ToastInterface } from "@/types/ToastInterface";
 
 const props = defineProps<{ toast: ToastInterface; toastStyle?: string }>();
 </script>
 
 <style scoped>
-
 .list-enter-active {
   transition: all 0.5s ease;
 }
@@ -39,23 +28,5 @@ const props = defineProps<{ toast: ToastInterface; toastStyle?: string }>();
 .list-leave-to {
   opacity: 0;
   transform: translateX(30px);
-}
-
-.bar {
-  animation-name: bar;
-  animation-timing-function: linear;
-  animation-fill-mode: forwards;
-  background-color: #39b883;
-  width: 0;
-}
-
-@keyframes bar {
-  0% {
-    width: 0;
-  }
-
-  100% {
-    width: 100%;
-  }
 }
 </style>
